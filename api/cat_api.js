c214de32afa3ae2c1c6ff2ef93f4a070cafdd5a3
Hannah Stewart <hmstewart84@gmail.com>
2016-10-05 11:54:41 +0100

    One route API complete

--- api/cat_api.js
+++ api/cat_api.js
@@ -1,7 +1,20 @@
 var CatApi = function(app) {
 
+  var cats = [{
+    name: "Jolly",
+    breed: "Moggy"
+  },
+  {
+    name: "Garfield",
+    breed: "Orange"
+  }];
+
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+
+  app.get('/api/cats', function(req, res) {
+    res.json({data: cats});
   });
 
 }
